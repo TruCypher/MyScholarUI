31a1b7265cd4c39c9ac9076634091aac8e4ccbbb
Tru Nguyen <nguyenquoctru99@gmail.com>
2022-04-19 11:28:39 -0500

    added multiple element

--- src/User/Scholarship.js
+++ src/User/Scholarship.js
@@ -1,21 +1,77 @@
 
 function Scholarship () {
+    
+     const scholarShips = [
+        {
+            name: "Fafsa",
+            date: "May, 5 2022",
+            price: "$9000 recieveable",
+        },
+        {
+            name: "American Legion",
+            date: "May, 7 2022",
+            price: "$10,000 recieveable",
+        },
+        {
+            name: "AXA Achievement",
+            date: "June, 3 2022",
+            price: "$7000 recieveable",
+        },
+        {
+            name: "SHPE Scholar",
+            date: "April, 1 2022",
+            price: "$11,0000 recieveable",
+        },
+        {
+            name: "Do Something Scholarship",
+            date: "March, 20 2022",
+            price: "$100,0000 recieveable",
+        },
+        {
+            name: "Burger King Scholars",
+            date: "April, 25 2022",
+            price: "$25,000 recieveable",
+        },
+        {
+            name: "SHPE Scholar",
+            date: "April, 1 2022",
+            price: "$11,0000 recieveable",
+        },
+        {
+            name: "Elks Foundation Most Valuable Students Scholarship",
+            date: "May, 21 2022",
+            price: "$15,0000 recieveable",
+        },
+        {
+            name: "Doodle 4 Google",
+            date: "April, 12 2022",
+            price: "$30,0000 recieveable",
+        },
+    ]
+
     return (
         <div className='flex grow flex-col m-5 mr-10'>
             <div className='flex'>
                 <h1 className='text-xl text-yellow font-bold font-inter justify-center mb-5 pl-2'>Personalized Scholarship List</h1>
             </div>
             
-            <div className='flex justify-between border-4 border-green p-5 rounded-xl'>
-                <div>
-                    <h1 className='text-white text-2xl font-bold font-inter mb-4'>Fafsa</h1> 
-                    <p className='text-white font-inter'>$9000 Recievable</p>
-                </div>
-                <div>
-                    <p className='text-white font-inter mb-4'>Due May, 5 2022</p>
-                    <p className='text-white font-inter'>Essay Required</p>
-                </div> 
-            </div>
+            {
+                scholarShips.map(scholar => {
+                    return (
+                        <div className='flex justify-between border-4 border-green p-5 rounded-xl mb-8'>
+                            <div>
+                                <h1 className='text-white text-2xl font-bold font-inter mb-4'>{scholar.name}</h1> 
+                                <p className='text-white font-inter'>{scholar.price}</p>
+                            </div>
+                            <div>
+                                <p className='text-white font-inter mb-4'>{scholar.date}</p>
+                                <p className='text-white font-inter'>Essay Required</p>
+                            </div> 
+                        </div>
+                    )
+                })
+            }
+            
         </div>
         )
 }
--- src/User/Wizard.js
+++ src/User/Wizard.js
@@ -30,70 +30,139 @@ function Wizard() {
 }
 
 function ScholarDueSoon() {
+
+    const scholarShips = [
+        {
+            name: "Fafsa",
+            date: "May, 5 2022",
+            price: "$9000 recieveable",
+        },
+        {
+            name: "American Legion",
+            date: "May, 7 2022",
+            price: "$10,000 recieveable",
+        },
+        {
+            name: "AXA Achievement",
+            date: "June, 3 2022",
+            price: "$7000 recieveable",
+        },
+        {
+            name: "SHPE Scholar",
+            date: "April, 1 2022",
+            price: "$11,0000 recieveable",
+        },
+    ]
+
     return (
         <div className='flex grow flex-col m-5 mr-10'>
             <div className='flex justify-center'>
                 <h1 className='text-xl text-yellow font-bold font-inter justify-center mb-5'>Scholarship Due Soon</h1>
             </div>
             
-            <div className='flex justify-between border-4 border-green p-5 rounded-xl'>
-                <div>
-                    <h1 className='text-white text-2xl font-bold font-inter mb-4'>Fafsa</h1> 
-                    <p className='text-white font-inter'>$9000 Recievable</p>
-                </div>
-                <div>
-                    <p className='text-white font-inter mb-4'>Due May, 5 2022</p>
-                    <p className='text-white font-inter'>Essay Required</p>
-                </div> 
-            </div>
+            {
+                scholarShips.map(scholar => {
+                    return (
+                        <div className='flex justify-between border-4 border-green p-5 rounded-xl mb-5'>
+                            <div>
+                                <h1 className='text-white text-2xl font-bold font-inter mb-4'>{scholar.name}</h1> 
+                                <p className='text-white font-inter'>{scholar.price}</p>
+                            </div>
+                            <div>
+                                <p className='text-white font-inter mb-4'>{scholar.date}</p>
+                                <p className='text-white font-inter'>Essay Required</p>
+                            </div> 
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
 
 function AppliedScholarship() {
+
+    const scholarShips = [
+        {
+            name: "Fafsa",
+            date: "Submitted May, 5 2022",
+            price: "$9000 recieveable",
+        },
+        {
+            name: "American Legion",
+            date: "Submitted May, 7 2022",
+            price: "$10,000 recieveable",
+        },
+    ]
+
     return (
         <div className='flex grow flex-col m-5 mr-10'>
             <div className='flex justify-center'>
                 <h1 className='text-xl text-yellow font-bold font-inter justify-center mb-5'>Applied Scholarship</h1>
             </div>
             
-            <div className='flex justify-between border-4 border-green p-5 rounded-xl'>
-                <div>
-                    <h1 className='text-white text-2xl font-bold font-inter mb-4'>Fafsa</h1> 
-                    <p className='text-white font-inter'>$9000 Recievable</p>
-                </div>
-                <div>
-                    <p className='text-white font-inter mb-4'>Submitted May, 5 2022</p>
-                </div> 
-            </div>
+            {
+                scholarShips.map(scholar => {
+                    return (
+                        <div className='flex justify-between border-4 border-green p-5 rounded-xl mb-5'>
+                            <div>
+                                <h1 className='text-white text-2xl font-bold font-inter mb-4'>{scholar.name}</h1> 
+                                <p className='text-white font-inter'>{scholar.price}</p>
+                            </div>
+                            <div>
+                                <p className='text-white font-inter mb-4'>{scholar.date}</p>
+                            </div> 
+                        </div>
+                    )
+                })
+            }
+            
         </div>
     )
 }
 
 function MyEssay() {
+
+    const scholarShips = [
+        {
+            name: "Fafsa",
+            date: "Submitted May, 5 2022",
+        },
+        {
+            name: "American Legion",
+            date: "Submitted May, 7 2022",
+        },
+    ]
+
     return (
         <div className='flex grow flex-col m-5 mr-10'>
             <div className='flex justify-center'>
                 <h1 className='text-xl text-yellow font-bold font-inter justify-center mb-5'>My Essays</h1>
             </div>
             
-            <div className='border-4 border-green p-5 rounded-xl'>
-                <div className='flex justify-between'>
-                    <div>
-                        <h1 className='text-white text-2xl font-bold font-inter mb-4'>Fafsa</h1> 
-                    </div>
-                    <div>
-                        <p className='text-white font-inter mb-4'>Submitted May, 5 2022</p>
-                    </div> 
-                </div>
-                <p className='text-white font-inter'>
-                    Essays: Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type 
-                    specimen book. It has survived not only five centuries,
-                </p>
-                
-            </div>
+            {
+                scholarShips.map(scholar => {
+                    return (
+                        <div className='border-4 border-green p-5 rounded-xl mb-5'>
+                            <div className='flex justify-between'>
+                                <div>
+                                    <h1 className='text-white text-2xl font-bold font-inter mb-4'>{scholar.name}</h1> 
+                                </div>
+                                <div>
+                                    <p className='text-white font-inter mb-4'>{scholar.date}</p>
+                                </div> 
+                            </div>
+                            <p className='text-white font-inter'>
+                                Essays: Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type 
+                                specimen book. It has survived not only five centuries,
+                            </p>
+                        </div>
+                    )
+                })
+            }
+            
         </div>
     )
 }
